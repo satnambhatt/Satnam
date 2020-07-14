19be51c93197131ca07de8f1c6c3538823f5140a
satnambhatt <satnam@SatnamPC.localdomain>
2020-07-14 23:34:32 +1200

    Updates

---
+++ precache-manifest.3e0e0bcff19d5c3b577b368c95853b47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921402cc530f3175a2d74bb3539b1eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5883d06471a86506698f",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "de53fa91cada8ae4c01a",
+    "url": "/static/css/main.20e1eaea.chunk.css"
+  },
+  {
+    "revision": "5883d06471a86506698f",
+    "url": "/static/js/2.6c64b110.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.6c64b110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de53fa91cada8ae4c01a",
+    "url": "/static/js/main.870e86eb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "3f3d2d2b255cb58ab003f93d6548e68b",
+    "url": "/static/media/pic1.3f3d2d2b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Satnam/precache-manifest.a1b61e38563b6cd795bdd7d972bf8ff9.js"
+  "/precache-manifest.3e0e0bcff19d5c3b577b368c95853b47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Satnam/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
